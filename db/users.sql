31e8c02fd87bad15d94d0948a76b78a22c21dd71
Tanjil <tanjilkh@gmail.com>
2024-06-27 15:02:17 +0100

    Update users.sql

--- db/users.sql
+++ db/users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO INCREMENT,
+    id INT PRIMARY KEY NOT NULL,
     identification_number INT NOT NULL,
-    password VARCHAR(255) NOT NULL,
-)
+    password VARCHAR(255) NOT NULL
+);
