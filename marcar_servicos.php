941b4d3ba06d2e970a7585edd9e5dee6f87d41b8
Tanjil <tanjilkh@gmail.com>
2024-06-28 15:26:22 +0100

    Update marcar_servicos.php

--- marcar_servicos.php
+++ marcar_servicos.php
@@ -122,6 +122,13 @@ $user_id = $_SESSION['user_id'];
                   
                 </li>
                 <li class="nav-item">
+                <?php
+                  if(isset($_SESSION['user_id'])) {
+                    echo '<a class="nav-link" href="marcar_servicos.php">Marcar Serviço</a>';
+                  }
+                  ?>
+                </li>
+                <li class="nav-item">
                   <a class="nav-link" href="#servicos">Servicos</a>
                 </li>
                 <li class="nav-item">
