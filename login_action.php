b6f10283f3b79db97263c7db4085b5f6f77722b9
Tanjil <tanjilkh@gmail.com>
2024-06-28 15:21:07 +0100

    Update login_action.php
    ir para index

--- login_action.php
+++ login_action.php
@@ -16,7 +16,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         if($user['password'] === $password){
             $_SESSION['user_id'] = $user['id'];
-            header('Location: welcome.php');
+            header('Location: index.php');
         } else {
             echo "Pass errada";
         }
