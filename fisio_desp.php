1f58a3557b0d8f5dfb9da74589420a4c89bde936
RicardoMaga <113973409+RicardoMaga@users.noreply.github.com>
2024-07-02 22:48:58 +0100

    Mudanças na pagina
    background  e navbar
    Ainda vou mudar mais coisas

--- fisio_desp.php
+++ fisio_desp.php
@@ -32,11 +32,34 @@ if (isset($_SESSION['user_id'])) {
   <link href="css/style.css" rel="stylesheet" />
   <!-- responsive style -->
   <link href="css/responsive.css" rel="stylesheet" />
+  <style>
+    .content-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 50px;
+      color: white; 
+    }
+    .content-container .text {
+      flex: 1;
+      padding-right: 20px;
+      font-size: 30px;
+    }
+    .content-container .image {
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;
+    }
+    .content-container .image img {
+      max-width: 100%;
+      border-radius: 20px;
+    }
+  </style>
 </head>
 
 <body>
-  <div class="hero_area">
-    <!-- header section strats -->
+  <div class="hero_area_fisio_desp">
+    <!-- header section starts -->
     <header class="header_section">
       <div class="container-fluid ">
         <nav class="navbar navbar-expand-lg custom_nav-container ">
@@ -52,10 +75,9 @@ if (isset($_SESSION['user_id'])) {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="ml-auto">
               <ul class="navbar-nav  ">
-  
                 <li class="nav-item">
-                <?php
-                  if(!isset($_SESSION['user_id'])) {
+                  <?php
+                  if (!isset($_SESSION['user_id'])) {
                     echo '<a class="nav-link" href="login.html">Log In</a>';
                   } else {
                     echo '<a class="nav-link" href="logout.php">Log Out</a>';
@@ -63,7 +85,7 @@ if (isset($_SESSION['user_id'])) {
                   ?>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" href="marcar_servicos.php">Marcar Serviço</a>
+                  <a class="nav-link" href="marcar_servicos.php">Marcar Serviço</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#servicos">Servicos</a>
@@ -78,153 +100,31 @@ if (isset($_SESSION['user_id'])) {
       </div>
     </header>
     <!-- end header section -->
-    <!-- slider section -->
-    <section class=" slider_section position-relative">
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
-                  <div>
-                    <h3>
-                      Fitness
-                    </h3>
-                    <h2>
-                      Serviços
-                    </h2>
-                    <h1>
-                      Six Pack Academy
-                    </h1>
-                    <p>
-                    A fisioterapia desportiva é uma especialidade da fisioterapia focada na prevenção, diagnóstico, tratamento e reabilitação de lesões relacionadas à prática de esportes e atividades físicas. Esse ramo da fisioterapia é essencial para atletas e praticantes de atividades físicas, pois visa otimizar o desempenho esportivo e garantir uma recuperação rápida e eficaz após lesões.
-<br>
-Os fisioterapeutas desportivos utilizam uma combinação de técnicas manuais, exercícios terapêuticos, eletroterapia e outras modalidades para tratar problemas musculoesqueléticos, como entorses, distensões, fraturas e lesões de ligamentos e tendões. Além disso, eles desempenham um papel crucial na prevenção de lesões, oferecendo orientação sobre técnicas de treino adequadas, aquecimento, alongamento e fortalecimento muscular.
-<br>
-Outro aspecto importante da fisioterapia desportiva é o trabalho em equipe com outros profissionais de saúde, como médicos, treinadores e nutricionistas, para fornecer um cuidado integral ao atleta. Isso assegura que o tratamento seja personalizado e direcionado às necessidades específicas de cada indivíduo, ajudando-os a retornar ao esporte de maneira segura e eficiente. Em resumo, a fisioterapia desportiva é fundamental para promover a saúde e a performance dos atletas,
- bem como para minimizar o risco de lesões e otimizar a reabilitação quando estas ocorrem.
- <br>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-      
-      </div>
-    </section>
-    <!-- end slider section -->
+
+  <div class="content-container">
+    <div class="text">
+      <h1>Sobre a Fisioterapia Desportiva</h1>
+      <br><br>
+      <p>
+        A fisioterapia desportiva é uma especialidade da fisioterapia que atua na prevenção e tratamento de lesões relacionadas com a prática desportiva.
+        O principal objetivo é ajudar os atletas a recuperar rapidamente de lesões e a melhorar a sua performance.
+      </p>
+    </div>
+    <div class="image">
+      <img src="images/fisio_desp2.png" alt="Fisioterapia Desportiva">
+    </div>
+  </div>
   </div>
 
 
   <!-- Us section -->
 
-  <section class="us_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2 id="servicos">
-          Os nossos serviços
-        </h2>
-      </div>
-
-      <div class="us_container ">
-        <div class="row">
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/physiotherapist.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  FISIOTERAPIA DESPORTIVA
-                </h5>
-                <p>
-                  Tratamento e prevenção de lesões relacionadas com a prática de atividade física e desporto.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/dumbell.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  REABILITAÇÃO DESPORTIVA
-                </h5>
-                <p>
-                  Processo de recuperação de lesões ou doenças que afetam a performance atlética.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/av_postural.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  AVALIAÇÃO POSTURAL
-                </h5>
-                <p>
-                  Análise da postura e do alinhamento do corpo para identificar desequilíbrios e anomalias.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/massage.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  DRENAGEM LINFÁTICA
-                </h5>
-                <p>
-                  Massagem que ajuda a estimular o sistema linfático e promover a eliminação de toxinas do corpo.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
   <!-- end us section -->
-
 
   <!-- heathy section -->
 
-  <section class="heathy_section layout_padding">
-    <div class="container">
-
-      <div class="row">
-        <div class="col-md-12 mx-auto">
-          <div class="detail-box">
-            <h2>
-              HEALTHY MIND, HEALTHY BODY
-            </h2>
-            <p>
-              No Ginásio Six Pack, valorizamos a conexão entre a mente e o corpo. 
-              Uma mente saudável é essencial para um corpo saudável. 
-              Oferecemos serviços que promovem o equilíbrio mental e físico, ajudando-o a viver de forma mais harmoniosa e saudável. 
-              Invista no seu bem-estar integral connosco.            
-            </p>
-
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </section>
-
   <!-- end heathy section -->
 
-  
   <!-- contact section -->
 
   <section class="contact_section ">
@@ -318,7 +218,7 @@ Outro aspecto importante da fisioterapia desportiva é o trabalho em equipe com 
   <!-- footer section -->
   <footer class="container-fluid footer_section">
     <p>
-      &copy; 2024 All Rights Reserved. 
+      &copy; 2024 All Rights Reserved.
       <a href="https://www.consumidor.gov.pt/livro-de-reclamacoes.aspx">Livro de reclamações</a>
     </p>
   </footer>
