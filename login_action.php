ca2e48332ef839af89fc0837fd6aa35c3d37780f
Tanjil <tanjilkh@gmail.com>
2024-07-11 09:57:05 +0100

    Update login_action.php
    carregar a variavel cargo para autorizacao

--- login_action.php
+++ login_action.php
@@ -17,7 +17,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         if($user['password'] === $password){
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['identification_number'] = $user['identification_number'];
-            
+            $_SESSION['role'] = $user[role];
             header('Location: index.php');
         } else {
             echo "Pass errada";
