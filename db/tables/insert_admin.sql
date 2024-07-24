8ebd17dfb8540a776f163e4bb05c91136dfbf736
Tanjil <tanjilkh@gmail.com>
2024-07-24 12:16:57 +0100

    Create insert_admin.sql

---
+++ db/tables/insert_admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (
+	id, username, first_name, last_name, email, phone_number, identification_number, password, role
+	) VALUES (999, 'admin', 'admin', 'admin', 'admin@sixpack.com', '+351 914 720 543', 0000, 'admin', 1);
