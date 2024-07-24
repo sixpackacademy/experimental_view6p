fcef8ae27d50744d5231f035085c0550638adac6
Tanjil <tanjilkh@gmail.com>
2024-07-24 14:39:34 +0100

    Update loja.php

--- loja.php
+++ loja.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en" >
-<head>
+<head><!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
 	<style>
 		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
 
@@ -25,7 +36,7 @@ a:hover {
 
 .section-products .header h3 {
     font-size: 1rem;
-    color: #fe302f;
+    color: #2E7D32;
     font-weight: 500;
 }
 
@@ -158,6 +169,54 @@ a:hover {
 
 </head>
 <body>
+ <div class="container-fluid ">	
+	        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.php">
+            <span class="text-dark">
+              Six Pack Academy
+            </span>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="ml-auto">
+              <ul class="navbar-nav  ">
+  
+                
+                <?php
+                  if(!isset($_SESSION['user_id'])) {
+                    echo '<li class="nav-item">';
+                    echo '<a class="nav-link text-dark" href="login.php">Log In</a>';
+                    echo '</li>';
+                  } else {
+                    echo '<li class="nav-item">';
+                    echo '<a class="nav-link text-dark" href="logout.php">Log Out</a>';
+                    echo '</li>';
+                    echo '<li class="nav-item">';
+                    if($_SESSION['role'] == 1){
+                      echo '<a class="nav-link text-dark" href="/dashboard">Dashboard</a>';
+                    }
+                    echo '</li>';
+                    echo '<li class="nav-item">';
+                    echo '<a class="nav-link text-dark" href="profile.php"> Profile</a>';
+                    echo '</li>';
+                  }
+                  ?>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-dark" href="loja.php">Loja</a>
+                </li>
+               
+               
+                
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+    </div>
 <!-- partial:index.partial.html -->
 <section class="section-products">
 		<div class="container">
