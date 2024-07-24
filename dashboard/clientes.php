c5ee70e3965a6d822ee64299dc9ab981fe4e3afa
Tanjil <tanjilkh@gmail.com>
2024-07-24 14:43:09 +0100

    Update clientes.php

--- dashboard/clientes.php
+++ dashboard/clientes.php
@@ -45,6 +45,9 @@ if ($role == 1) {
                 <li>
                     <a href="/dashboard/clientes.php" class="active" id="accounts"><i class='bx bxs-user-account'></i><span>Clientes</span></a>
                 </li>
+                <li>
+                    <a href="/dashboard/produtos.php" class="" id="accounts"><i class='bx bxs-user-account'></i><span>Produtos</span></a>
+                </li>
                 
                 <li>
                     <a href="/dashboard/contactos.php" class="" id="reviews"><i class='bx bxs-comment-detail'></i><span>Contactos</span></a>
@@ -71,7 +74,7 @@ if ($role == 1) {
             <div class="reviews">
                 <div class="recent-reviews">
                     <div class="header">
-                        <h2>Clientes Recentes</h2>
+                        <h2>Clientes</h2>
                     </div>
                     <table>
                         <thead>
